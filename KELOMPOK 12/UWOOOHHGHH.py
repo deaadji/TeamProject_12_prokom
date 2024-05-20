e3fe10fd0535dfbc1fb72f6c78b8bdd01e539189
Adji Legion <deaadji@gmail.com>
2024-05-20 14:16:49 +0700

    14.16, senin, adji, fungsi register, window register, main root

--- KELOMPOK 12/UWOOOHHGHH.py
+++ KELOMPOK 12/UWOOOHHGHH.py
@@ -10,9 +10,31 @@ root.configure(bg="#fff")
 root.resizable(False,False)
 
 #fungsi utama login screen
-def daftar_CMD():
+def login_CMD():
     usernama=username.get()
     paswed=password.get()
+
+    if usernama=='admoon' and paswed=='halo dunia':
+        print("ahayy")
+        Main_menu()
+    elif usernama!='admoon' and paswed!='halo dunia':
+        messagebox.showerror("Tidak Ditemukan","Username dan/atau Password salah. pastikan kapital dan spasi tepat")
+
+    with open("users.csv", mode="r") as f:
+        reader = csv.reader(f,delimiter=",")
+        for row in reader:
+            if row == [usernama,paswed]:
+                Main_menu()
+                return True
+    messagebox.showerror("Tidak Ditemukan","Username dan/atau Password salah. pastikan kapital dan spasi tepat")
+    return False
+
+    if usernama=='admoon' and paswed=='halo dunia':
+        print("ahayy")
+        Main_menu()
+    elif usernama!='admoon' and paswed!='halo dunia':
+        messagebox.showerror("Tidak Ditemukan","Username dan/atau Password salah. pastikan kapital dan spasi tepat")
+
 
     if usernama=='admoon' and paswed=='halo dunia':
         print("ahayy")
@@ -35,7 +57,68 @@ def Main_menu():
     Label(screen1,text='Awikwok',bg='white', font=('Calibri(body)',50,'bold')).pack(expand=True)
 
     screen1.mainloop()
+
+
+
+
+
+
+#=======================================================
+# MAIN REGISTER WINDOW
+
+def register():
+    daftar_screen=Toplevel(root)
+    daftar_screen.title("DAFTAR")
+    daftar_screen.geometry('625x500')
+    daftar_screen.config(bg='white')
+
+    def on_enter(e):
+        username.delete(0, "end")
+
+    def on_leave(e):
+        name=username.get()
+        if name=='':
+            username.insert(0,'Tulis Username Anda')
     
+    Frame(daftar_screen,width=300,height=2,bg='black').place(x=165,y=270)
+    Frame(daftar_screen,width=300,height=2,bg='black').place(x=165,y=340)
+
+    username = Entry(daftar_screen,width=25,fg='grey',border=0,bg='white',font=('Microsoft YaHei UI Light',13))
+    username.place(x=180,y=240)
+    username.insert(0,'Tulis Username Anda')
+    username.bind('<FocusIn>', on_enter)
+    username.bind('<FocusOut>', on_leave)
+
+    def on_enter(e):
+        password.delete(0, "end")
+
+    def on_leave(e):
+        name=username.get()
+        if name=='':
+            password.insert(0,'Tulis Password Anda')
+    password = Entry(daftar_screen,width=25,fg='grey',border=0,bg='white',font=('Microsoft YaHei UI Light',13))
+    password.place(x=180,y=310)
+    password.insert(0,'Tulis Password Anda')
+    password.bind('<FocusIn>', on_enter)
+    password.bind('<FocusOut>', on_leave)
+
+    with open("users.csv", mode="a", newline="") as f:
+        writer = csv.writer(f,delimiter=",")
+        email= username.get
+        passuword = password.get()
+        def daftar_fungsi():
+            if username=="":
+                messagebox.showerror("Invalid","Masukan Username dan Password terlebih dahulu")
+            elif password=="":
+                messagebox.showerror("Invalid","Masukan Username dan Password terlebih dahulu")
+            else:
+                writer.writerow(email,passuword)
+        daftar=Button(daftar_screen,width=16,text='DAFTAR',border=0,bg='white',cursor='hand2',font=('Microsoft YaHei UI Light',8,'bold'), command=daftar_fungsi)
+        daftar.place(x=244,y=370)
+        #print("Registration is succesful!")
+        ##   print("Please try again!")
+    
+
 
 
 #===================================================================
@@ -44,7 +127,7 @@ def Main_menu():
 
 
 #bug disini, janlup directory nya
-gambar_kiri=PhotoImage(file='TeamProject_12_prokom/KELOMPOK 12/LOGO LOGIN.png')
+gambar_kiri=PhotoImage(file='KELOMPOK 12/LOGO LOGIN.png')
 
 frame=Frame(root,width=350,height=350,bg='white')
 frame.place(x=480,y=70)
@@ -83,10 +166,10 @@ password.bind('<FocusOut>', on_leave)
 
 Frame(frame,width=300,height=2,bg='black').place(x=25,y=110)
 Frame(frame,width=300,height=2,bg='black').place(x=25,y=170)
-Button(frame,width=39,pady=7,text='sign in',bg='#de2b28',cursor='hand2',fg='white',border=0,command=daftar_CMD).place(x=35,y=205)
+Button(frame,width=39,pady=7,text='sign in',bg='#de2b28',cursor='hand2',fg='white',border=0,command=login_CMD).place(x=35,y=205)
 akun=Label(frame,text='Tidak memiliki akun SI JAGO PARKIR?',fg='black',bg='white',font=('Microsoft YaHei UI Light',9))
 akun.place(x=62,y=250)
-daftar=Button(frame,width=16,text='Daftar sekarang!',border=0,bg='white',cursor='hand2',font=('Microsoft YaHei UI Light',8,'bold'))
+daftar=Button(frame,width=16,text='Daftar sekarang!',border=0,bg='white',cursor='hand2',font=('Microsoft YaHei UI Light',8,'bold'), command=register)
 daftar.place(x=110,y=270)
 
 copy=Label(root,text='Kelompok 12 Nexus Copyright @2024',fg='navy',border=0,bg='white',font=('Microsoft YaHei UI Light',9,'italic'))
